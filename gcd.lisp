1a0f3fe559f23dd8c663628a16a8713a6db94d02
George Kettleborough <g.kettleborough@member.fsf.org>
2010-10-02 15:31:22 +0100

    Add subresultant GCD algorithm

--- gcd.lisp
+++ gcd.lisp
@@ -29,7 +29,7 @@
   (2arg-gcd poly2 poly1))
 
 (defmethod 2arg-gcd ((poly1 polynomial) (poly2 polynomial))
-  (wwgcd poly1 poly2 (poly-highest-var poly1 poly2)))
+  (subresultantgcd poly1 poly2 (poly-highest-var poly1 poly2)))
 
 ;; WWGCD works before I can be bothered to implement a better one
 (defun wwgcd (a b var)
@@ -43,3 +43,39 @@
          (setf d (pp r var)))
     (setf gamma (gcd (content a var) (content b var)))
     (* gamma c)))
+
+;; subresultant gcd algorithm
+(defun subresultantgcd (a b var)
+  (let ((c a)
+        (d b)
+        (r 0)
+        (beta)
+        (gamma)
+        (delta (- (deg a var) (deg b var)))
+        (psi -1))
+    ;; (format t "R_0 = ~a~%R_1 = ~a~%" c d)
+    ;; (format t "   psi_1 = ~a~%" psi)
+    ;; (format t "   delta_1 = ~a~%" delta)
+    (setf beta (^ -1 (1+ delta)))
+    ;; (format t "   beta_1 = ~a~%" beta)
+    (loop for i = 2 then (1+ i)
+       while (not (zerop d)) do
+         (setf r (prem c d))
+         (setf c d)
+         (setf d r)
+         ; remove beta
+         (when (not (zerop beta))
+           (setf d (/ d beta)))
+         ;; (format t "R_~a = ~a~%" i d)
+         ; calculate new beta
+         (setf psi (* (^ (- (lc c var)) delta)
+                      (^ psi (- 1 delta))))
+         ;; (format t "   psi_~a = ~a~%" i psi)
+         (setf delta (- (deg c var) (deg d var)))
+         ;; (format t "   delta_~a = ~a~%" i delta)
+         (setf beta (* (- (lc c var))
+                       (^ psi delta)))
+         ;; (format t "   beta_~a = ~a~%" i beta)
+         )
+    (setf gamma (gcd (content a var) (content b var)))
+    (* gamma (pp c var))))
